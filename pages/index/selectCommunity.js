0cee4120193e182e2ac38a8923c633b0039574ff
forrest23 <forrest23@126.com>
2016-09-28 17:55:17 +0800

    从接口获取数据

--- pages/index/selectCommunity.js
+++ pages/index/selectCommunity.js
@@ -1,69 +1,64 @@
 var app = getApp();
 var requester = app.globalData.requester;
-Page( {
+Page({
     data: {
-        array: [ '上海', '江苏', '浙江', '安微' ],
+        array: ['上海', '南京', '浙江', '安微'],
         index: 0,
         src: '../../image/arrowdown.png',
         homeSrc: '../../image/home.png',
-        items: [ {
-            id: 0,
-            name: '华兴园',
-            address: '华凌西路1225弄'
-        },
-            {
-                id: 1,
-                name: '爱博二村',
-                address: '盘古路200号'
-            },
-            {
-                id: 2,
-                name: '巴黎花园',
-                address: '虹莘路1935弄1～7号'
-            },
-            {
-                id: 3,
-                name: '保安新村',
-                address: '报春路851号'
-            }, {
-                id: 4,
-                name: '宝钢六村小区',
-                address: '友谊路221号附近'
-            }]
+        movies: [],
+        loading: false,
     },
-    bindAddressTap: function( e ) {
+    bindAddressTap: function (e) {
         var id = e.currentTarget.id;
-        for( var key in this.data.items ) {
-            if( key == id ) {
-                app.globalData.communityName = this.data.items[ key ].name;
+        for (var key in this.data.movies) {
+            if (this.data.movies[key].id == id) {
+                app.globalData.communityName = this.data.movies[key].name;
                 wx.navigateBack();
             }
         }
     },
-    bindInputBlur: function( e ) {
-        this.setData( {
-            items: [ {
-                name: '学府涵青',
-                address: '涵青路398弄'
-            },
-                {
-                    name: '华兴园',
-                    address: '华凌西路1225弄华凌西路1225弄'
-                },
-            ]
-        })
+    search: function (e) {
+        var where = '{}';
+        if (e.detail.value) where = '{"name": {"like": "' + e.detail.value + '"}}';
+
+        this.setData({ loading: true })
+
+        requester.getCommunity('community', 0, 10, where)
+            .then(d => {
+                this.setData({ movies: d, loading: false })
+            })
+            .catch(e => {
+                console.error(e)
+                this.setData({ movies: [], loading: false })
+            })
     },
-    bindPickerChange: function( e ) {
-        console.log( 'picker发送选择改变，携带值为', e.detail.value );
-        this.setData( {
-            index: e.detail.value
+    bindPickerChange: function (e) {
+        console.log('picker发送选择改变，携带值为', e.detail.value);
+        this.setData({
+            index: e.detail.value,
+            loading: true
         })
+
+        var where = '{"city": "' + this.data.array[e.detail.value] + '"}';
+        requester.getCommunity('community', 0, 10, where)
+            .then(d => {
+                this.setData({ movies: d, loading: false })
+            })
+            .catch(e => {
+                console.error(e)
+                this.setData({ movies: [], loading: false })
+            })
     },
-    onLoad: function() {
-        // requester.getBook( '1212', function( data ) {
-        //     console.log( data );
-        // }, function( code, msg ) {
-        //     console.log( code + ":" + msg );
-        // });
+    onLoad: function () {
+        var where = '{"city": "' + this.data.array[0] + '"}';
+        requester.getCommunity('community', 0, 10, where)
+            .then(d => {
+                this.setData({ movies: d, loading: false })
+            })
+            .catch(e => {
+                console.error(e)
+                this.setData({ movies: [], loading: false })
+            })
     }
 })
