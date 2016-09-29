94f5825db45703cc7ae942ba49cef9ccce5e3db2
forrest23 <forrest23@126.com>
2016-09-29 17:35:02 +0800

    增加新增按钮

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,30 +1,48 @@
 //index.js
 //获取应用实例
 var app = getApp()
-Page( {
+Page({
   data: {
     text: app.globalData.communityName,
-    src: '../../image/arrowdown.png'
+    actionSheetHidden: true,
+    actionSheetItems: ['新鲜事', '求助', '服务', '团购', '二手买卖']
   },
-  tapHead: function( event ) {
-    wx.navigateTo( {
+
+  tapAdd: function (event) {
+    this.setData({ actionSheetHidden: false });
+  },
+
+  tapHead: function (event) {
+    wx.navigateTo({
       url: '../index/selectCommunity'
     })
   },
-  onLoad: function() {
-    console.log( 'onLoad' );
-    var value = wx.getStorageSync( 'communityName' );
-    if( value ) {
-      this.setData( {
+
+  actionSheetChange: function (e) {
+    this.setData({
+      actionSheetHidden: !this.data.actionSheetHidden
+    })
+  },
+
+  bindItemTap: function (e) {
+    console.log('tap ' + e.currentTarget.dataset.name)
+  },
+
+  onLoad: function () {
+    console.log('onLoad');
+    var value = wx.getStorageSync('communityName');
+    if (value) {
+      this.setData({
         text: value
       });
       app.globalData.communityName = value;
     }
   },
-  onShow: function() {
-    console.log( 'onShow' );
-    if( this.data.text != app.globalData.communityName ) {
-      this.setData( {
+
+  onShow: function () {
+    console.log('onShow');
+    if (this.data.text != app.globalData.communityName) {
+      this.setData({
         text: app.globalData.communityName
       })
     }
